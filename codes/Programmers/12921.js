7f87978149c26ffc5d519828649cf265468a79c3
17-sss <xzxking17@gmail.com>
2021-03-08 18:31:14 +0900

    [Fix] Programmers 12921

--- codes/Programmers/12921.js
+++ codes/Programmers/12921.js
@@ -1,2 +1,18 @@
-console.log(Math.sqrt(2));
-// https://ehpub.co.kr/tag/1%EB%B6%80%ED%84%B0-1000-%EC%82%AC%EC%9D%B4%EC%97%90-%EC%86%8C%EC%88%98-%EC%B0%BE%EA%B8%B0/
+// https://programmers.co.kr/learn/courses/30/lessons/12921
+// 소수 찾기
+
+// 3차시, 통과 성공 (참고해서 품, 다른 방법으로도 풀어보기)
+const solution = (n) => {
+    const arr = Array.from({ length: n }, (_, i) => i + 1);
+    arr[0] = 0;
+    for (let i = 2; i ** 2 <= n; i++) {
+        if (arr[i - 1]) {
+            for (let j = i * i; j <= n; j += i) {
+                arr[j - 1] = 0;
+            }
+        }
+    }
+
+    const count = arr.filter((v) => v).length;
+    return count;
+};
