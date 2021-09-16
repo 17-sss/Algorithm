228152c9040d6317206a8ff2d85a38efd8a46e46
Hoyoung Son <xzxking17@gmail.com>
2021-09-16 15:56:41 +0900

    docs: :books: Add Programmers 1 file (17677)

--- codes/Programmers/Level2/17677_▵.js
+++ codes/Programmers/Level2/17677_▵.js
@@ -1,10 +1,101 @@
 // https://programmers.co.kr/learn/courses/30/lessons/17677
 // 2018 KAKAO BLIND RECRUITMENT : [1차] 뉴스 클러스터링
 
+// (2021.09.16) ---------------------
+
+// 2차시, 통과했지만.. 왜?
+const createZipHap = (aStr) => {
+  const result = [];
+  let nLoc = 0;
+  const str = aStr.toUpperCase();
+  while (nLoc + 2 <= str.length) {
+    const strSlice = str.slice(nLoc, nLoc + 2);
+    /[A-Z]{2}/.test(strSlice) && result.push(strSlice);
+    nLoc++;
+  }
+  return result;
+};
+
+// 이 부분 이해하도록 하기! 
+const getUnionIntersectCnt = (arr1, arr2) => {
+  const result = { union: 0, intersect: 0 };
+  arr1.forEach((value) => {
+    if (arr2.includes(value)) {
+      arr2.splice(arr2.indexOf(value), 1);
+      result.intersect++;
+    }
+    result.union++;
+  });
+  arr2.forEach((_) => result.union++);
+  return result;
+};
+
+function solution(str1, str2) {
+  const arrStr1 = createZipHap(str1);
+  const arrStr2 = createZipHap(str2);
+  const { union, intersect } = getUnionIntersectCnt(arrStr1, arrStr2);
+  const MAX_16BIT = 2 ** 16;
+  return union === 0 ? MAX_16BIT : Math.floor((intersect / union) * MAX_16BIT);
+}
+
+const pipeLog = (...funcs) => funcs.forEach((func) => console.log(func));
+pipeLog(
+  solution('FRANCE', 'french'), // 16384
+  solution('handshake', 'shake hands'), // 65536
+  solution('aa1+aa2', 'AAAA12'), // 43690
+  solution('E=M*C^2', 'e=m*c^2'), // 65536
+  solution('aa', 'aaa'),
+  solution('abccc', 'ccdefgg'),
+);
+
+
+// 1차시, 또 실패
+/* 
+const createZipHap = (aStr) => {
+  const result = [];
+  let nLoc = 0;
+  const str = aStr.toUpperCase();
+  while (nLoc + 2 <= str.length) {
+    const strSlice = str.slice(nLoc, nLoc + 2);
+    /[A-Z]{2}/.test(strSlice) && result.push(strSlice);
+    nLoc++;
+  }
+  return result;
+};
+
+// const createIntersect = (arr1, arr2) => arr1.filter((v) => arr2.includes(v)); (이거 사용할떄가 점수 더 높음)
+const createIntersect = (arr1, arr2) => {
+  const result = [];
+  const first = arr1.length > arr2.length ? arr1 : arr2;
+  const second = arr1.length > arr2.length ? arr2 : arr1;
+  for (let i = 0; i < first.length; i++) {
+    const item = first[i];
+    second.includes(item) && result.push(item);
+  }
+  return result;
+}
+const createUnion = (arr1, arr2) => [...arr1, ...arr2];
+const getIntersectUnionCnt = (arr1, arr2) => {
+  const arrUnion = createUnion(arr1, arr2);
+  const arrIntersect = createIntersect(arr1, arr2);
+  const intersect = arrIntersect.length;
+  return { union: arrUnion.length - intersect, intersect} ;
+};
+
+function solution(str1, str2) {
+  const arrStr1 = createZipHap(str1);
+  const arrStr2 = createZipHap(str2);
+  const { union, intersect } = getIntersectUnionCnt(arrStr1, arrStr2);
+  const MAX_16BIT = 2 ** 16;
+  return union === 0 ? MAX_16BIT : Math.floor(intersect / union * MAX_16BIT);
+}
+*/
+
+
 // (2021.09.09) ---------------------
 
 // 4차시, 참고 (https://velog.io/@kimtaeeeny/프로그래머스-뉴스-클러스터링-javascript-2018-카카오공채)
-
+/* 
 function solution(str1, str2) {
   // [1] 각 문자의 집합을 생성
   const createZipHap = (str1, str2) =>
@@ -49,14 +140,7 @@ function solution(str1, str2) {
 
   return Math.floor((intersect.length / union.length ) * MAX_16BIT);
 }
-
-const pipeLog = (...funcs) => funcs.forEach((func) => console.log(func));
-pipeLog(
-  solution('FRANCE', 'french'), // 16384
-  solution('handshake', 'shake hands'), // 65536
-  solution('aa1+aa2', 'AAAA12'), // 43690
-  solution('E=M*C^2', 'e=m*c^2'), // 65536
-);
+*/
 
 // 3차시, 통과 실패.... 하... (테케 4, 5, 7, 9, 10, 11, 13)
 /* 
