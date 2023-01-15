352c827a869f3e159b2504ad39a519bb293c976e
Hoyoung Son <xzxking17@gmail.com>
2023-01-15 15:48:44 +0900

    docs: Modify IF - JS Algorithm 3 files
    - Section01, Section02의 일부 파일 Indent 수정

--- codes/Inflearn-JS_Algorithm/Section01/07-10부제/JS07.js
+++ codes/Inflearn-JS_Algorithm/Section01/07-10부제/JS07.js
@@ -1,10 +1,10 @@
 // ME
 function solution(day, arr) {
-    return arr.reduce((result, value) => {
-        const lastNum = value % 10;
-        if (lastNum === day) result++;
-        return result;
-    }, 0);
+  return arr.reduce((result, value) => {
+    const lastNum = value % 10;
+    if (lastNum === day) result++;
+    return result;
+  }, 0);
 }
 
 arr = [25, 23, 11, 47, 53, 17, 33];
@@ -12,12 +12,11 @@ console.log(solution(3, arr));
 
 // ANSWER
 function solution(day, arr) {
-    let answer = 0;
-    for (let x of arr) {
-        if (x % 10 == day) answer++;
-    }
-
-    return answer;
+  let answer = 0;
+  for (let x of arr) {
+    if (x % 10 == day) answer++;
+  }
+  return answer;
 }
 
 arr = [25, 23, 11, 47, 53, 17, 33];
--- codes/Inflearn-JS_Algorithm/Section02/05-등수 구하기/JS05.js
+++ codes/Inflearn-JS_Algorithm/Section02/05-등수 구하기/JS05.js
@@ -1,28 +1,28 @@
 // ME
 function solution(arr) {
-    const rankArr = [...arr].sort((a, b) => b - a); // 순위 전용 arr, 내림차순으로 정렬
-    return arr.map((v) => {
-        const rank = rankArr.indexOf(v);
-        return rank > -1 ? rank + 1 : rank;
-    });
+  const rankArr = [...arr].sort((a, b) => b - a); // 순위 전용 arr, 내림차순으로 정렬
+  return arr.map((v) => {
+    const rank = rankArr.indexOf(v);
+    return rank > -1 ? rank + 1 : rank;
+  });
 }
 
-let arr = [87, 89, 92, 100, 76];
-let arr2 = [87, 87, 100, 50, 42, 100, 87];
+const arr = [87, 89, 92, 100, 76];
+const arr2 = [87, 87, 100, 50, 42, 100, 87];
 console.log(solution(arr)); // [4, 3, 2, 1, 5]`
 console.log(solution(arr2)); // [3, 3, 1, 6, 7, 1, 3]
 
 // ANSWER
 function solution(arr) {
-    let n = arr.length;
-    let answer = Array.from({ length: n }, () => 1);
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (arr[j] > arr[i]) answer[i]++;
-        }
+  let n = arr.length;
+  let answer = Array.from({ length: n }, () => 1);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[j] > arr[i]) answer[i]++;
     }
-    return answer;
+  }
+  return answer;
 }
 
-let arr = [87, 89, 92, 100, 76];
+const arr = [87, 89, 92, 100, 76];
 console.log(solution(arr));
